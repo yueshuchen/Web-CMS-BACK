6e4816edf6859c9d287a07ed4c50bfbc6cf7791b
yueshuchen <32773383+yueshuchen@users.noreply.github.com>
2018-09-02 17:42:59 -0400

    application config

--- src/main/java/onlineShop/ApplicationConfig.java
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -9,14 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @Configuration
 public class ApplicationConfig {
 
 	@Bean(name = "sessionFactory")
 	public LocalSessionFactoryBean sessionFactory() {
+		
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource());
 		sessionFactory.setPackagesToScan("onlineShop.model");
@@ -29,18 +34,39 @@ public class ApplicationConfig {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		
-
+		// TODO: Change port according to your database settings
 		dataSource.setUrl("jdbc:mysql://localhost:3306/ecommerce?serverTimezone=UTC");
 		dataSource.setUsername("root");
 		dataSource.setPassword("root");
 
 		return dataSource;
 	}
+	
+    @Bean
+    public MultipartResolver multipartResolver() {
+   	 CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+   	 multipartResolver.setMaxUploadSize(10240000);
+   	 return multipartResolver;
+    }
+    
+    // Properly encode URL encoding for special characters.
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+    	StrictHttpFirewall firewall = new StrictHttpFirewall();
+    	firewall.setAllowUrlEncodedSlash(true);
+    	firewall.setAllowSemicolon(true);
+    	return firewall;
+    }
+    
+    private final Properties hibernateProperties() {
+      	 Properties hibernateProperties = new Properties();
+      	 hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+      	 hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+      	 return hibernateProperties;
+    }
+ 
 
-	private final Properties hibernateProperties() {
-		Properties hibernateProperties = new Properties();
-		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		return hibernateProperties;
-	}
+
 }
+
+
